672cef6b4071cf5f91228a5aeccfa271b5d00433
Tai @ Flex <dch.tai@gmail.com>
2016-10-31 11:48:16 +0000

    with textures and sounds

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- this is my silly mod
+
+minetest.register_node("sillymod:sillyblock",{
+	tiles = {"sillymod_sillyblock.png"},
+	groups = {choppy = 3},
+	sounds = {
+		footstep = "sillymod_step",
+	},
+})
